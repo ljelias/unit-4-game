f514e739a45011ded9a0a76fe746ff25407efe0b
ljelias <laura.j.elias@gmail.com>
2019-04-29 15:45:28 -0500

    JS mostly working but cant restart game without erasing wins and losses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,61 @@
-
-  let wins = 0;
-  let losses = 0;
-  let total = 0;
-  let message = "";
+let wins = 0;
+let losses = 0;
+let total = 0;
+let message = "";
 
 // CHOOSE random number
-  let randomNumber = Math.ceil(Math.random() * 120); 
-  console.log(randomNumber);
-  document.getElementById("randomnumber").innerHTML = randomNumber;
+let randomNumber = Math.ceil(Math.random() * 101) + 19; 
+console.log(randomNumber);
+document.getElementById("randomnumber").innerHTML = randomNumber;
 
-  let gemNum1 = Math.ceil(Math.random() * 12); 
-  console.log(gemNum1);
+let gemNum1 = Math.ceil(Math.random() * 12); 
+console.log(gemNum1);
 
-  let gemNum2 = Math.ceil(Math.random() * 12); 
-  console.log(gemNum2);
+let gemNum2 = Math.ceil(Math.random() * 12); 
+console.log(gemNum2);
 
-  let gemNum3 = Math.ceil(Math.random() * 12); 
-  console.log(gemNum3);
+let gemNum3 = Math.ceil(Math.random() * 12); 
+console.log(gemNum3);
 
-  let gemNum4 = Math.ceil(Math.random() * 12); 
-  console.log(gemNum4);
+let gemNum4 = Math.ceil(Math.random() * 12); 
+console.log(gemNum4);
+
+  $("#gem1").on("click", function() { 
+      total += gemNum1;
+      document.getElementById("totalscore").innerHTML = total;
+      winOrLose ();
+  });
+
+  $("#gem2").on("click", function() { 
+      total += gemNum2;
+      document.getElementById("totalscore").innerHTML = total;
+      winOrLose ();
+  });
+
+  $("#gem3").on("click", function() { 
+      total += gemNum3;
+      document.getElementById("totalscore").innerHTML = total;
+      winOrLose ();
+  });
+
+  $("#gem4").on("click", function() { 
+      total += gemNum4;
+      document.getElementById("totalscore").innerHTML = total;
+      winOrLose ();
+  });
 
 
-if (total < randomNumber) {
-  do  {
-    $(".gem").on("click", function () {
-        total = total + value;
-    })
-}
-while (total < randomNumber);
-}
-else if (total >= randomNumber) {
-     winOrLose();
-}
-
-  function winOrLose () {
-    if (total === randomNumber) { 
-        wins++;
-        message = "YOU WIN!";
-        document.getElementById("wintotal").innerHTML = wins;
-        document.getElementById("msg").innerHTML = message;
-
-    } else if (total > randomNumber) {
-        losses++;
-        message = "YOU LOSE!";
-        document.getElementById("losstotal").innerHTML = losses;
-        document.getElementById("msg").innerHTML = message;
-    }
+function winOrLose () {
+  if (total === randomNumber) { 
+      wins++;
+      message = "YOU WIN!";
+      document.getElementById("wintotal").innerHTML = wins;
+      document.getElementById("msg").innerHTML = message;
   }
+  else if (total > randomNumber) {
+      losses++;
+      message = "YOU LOSE!";
+      document.getElementById("losstotal").innerHTML = losses;
+      document.getElementById("msg").innerHTML = message;
+  }
+}
