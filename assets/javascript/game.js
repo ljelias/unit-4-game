bdb0b85c42b5c69d54bc04584aa34596b3d0cbaa
ljelias <laura.j.elias@gmail.com>
2019-04-29 21:10:20 -0500

    it worksgit add .

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,6 +20,7 @@ console.log(gemNum3);
 let gemNum4 = Math.ceil(Math.random() * 12); 
 console.log(gemNum4);
 
+
   $("#gem1").on("click", function() { 
       total += gemNum1;
       document.getElementById("totalscore").innerHTML = total;
@@ -45,17 +46,30 @@ console.log(gemNum4);
   });
 
 
+function startGame () {
+    randomNumber = Math.ceil(Math.random() * 101) + 19;
+    gemNum1 = Math.ceil(Math.random() * 12);
+    gemNum2 = Math.ceil(Math.random() * 12);
+    gemNum3 = Math.ceil(Math.random() * 12);
+    gemNum4 = Math.ceil(Math.random() * 12);    
+    total = 0;
+    document.getElementById("totalscore").innerHTML = total;
+    document.getElementById("randomnumber").innerHTML = randomNumber;
+}
+
 function winOrLose () {
   if (total === randomNumber) { 
       wins++;
       message = "YOU WIN!";
       document.getElementById("wintotal").innerHTML = wins;
       document.getElementById("msg").innerHTML = message;
+      startGame ();
   }
   else if (total > randomNumber) {
       losses++;
       message = "YOU LOSE!";
       document.getElementById("losstotal").innerHTML = losses;
       document.getElementById("msg").innerHTML = message;
+      startGame ();
   }
 }
